c64bcbf81f047a584d1c8d0f996c8a59fc1dccdb
Aasawari0309 <aasawari309@gmail.com>
2025-05-15 17:30:24 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,6 @@
+#Personalized greeting using user's name
+
+a = input("Enter your first name: ")
+b = input("Enter your last name: ")
+
+print("Hello,S " + a +" " + b + "!" + "Welcome to the python program.")
