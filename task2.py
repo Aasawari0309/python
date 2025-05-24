e138cd68daa36c1355b10f4032e3931dad72851f
Aasawari0309 <aasawari309@gmail.com>
2025-05-24 18:38:09 +0530

    Delete task2.py

--- task2.py
+++
@@ -1,6 +0,0 @@
-#Personalized greeting using user's name
-
-a = input("Enter your first name: ")
-b = input("Enter your last name: ")
-
-print("Hello,S " + a +" " + b + "!" + "Welcome to the python program.")
